6fe102a34e0dd6af2f3f394675ae868af2036e59
Akira Sasaki <gclue.akira@gmail.com>
2018-12-11 12:05:41 +0900

    Update sample.

--- example/servo/servo.ino
+++ example/servo/servo.ino
@@ -1,6 +1,9 @@
 #include "FaBoPWM_PCA9685.h"
 
 FaBoPWM faboPWM;
+int pos = 0;
+int MAX_VALUE = 600;
+int MIN_VALUE = 300;
 
 void setup() {
   Serial.begin(115200);
@@ -9,9 +12,16 @@ void setup() {
     faboPWM.init(300);
   }
   faboPWM.set_hz(50);
-  faboPWM.set_channel_value(0, 400);
+  
 }
 
 void loop() {
-  
+  for (pos = MIN_VALUE; pos <= MAX_VALUE; pos += 1) { 
+    faboPWM.set_channel_value(0, pos); 
+    delay(15);                      
+  }
+  for (pos = 600; MAX_VALUE >= MIN_VALUE; pos -= 1) { 
+    faboPWM.set_channel_value(0, pos);              
+    delay(15);      
+  }
 }
